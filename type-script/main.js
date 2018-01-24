650cac769f21e806f583570090863fcda9b2a93a
antonioPedroNeto <antonio.pedro.neto@ccc.ufcg.edu.br>
2018-01-24 10:43:36 -0300

    Angular, ES6 2015, TypeScript

---
+++ type-script/main.js
@@ -0,0 +1,7 @@
+var minhaVar = "minha variavel";
+function minhaFun(x, y) {
+    return x + y;
+}
+//ES6 2015
+var num = 2;
+var PI = 3.14;
---
+++ type-script/main.ts
@@ -0,0 +1,46 @@
+var minhaVar = "minha variavel";
+
+function minhaFun(x, y) {
+
+	return x + y;
+
+}
+
+
+//ES6 2015
+let num = 2;
+
+const PI = 3.14;
+
+
+
+var numeros = [1,2,3];
+
+//function
+
+numeros.map(function(valor){
+
+ return valor * 2;
+
+});
+
+// arrow functions ES 2015
+
+numeros.map( valor => {
+
+	valor * 2;
+
+})
+
+//classes
+
+class Matematica {
+
+	soma(x,y){
+		return x + y;
+	}
+
+}
+
+
+var n1 : string = "123213";
